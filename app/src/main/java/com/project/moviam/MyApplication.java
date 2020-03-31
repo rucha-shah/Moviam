ae29d43be2504e9d291331f0ad5184691947ad86
ruchajagatkumarshah <rjshah33.rs@gmail.com>
2020-03-31 19:06:54 +0530

    Dagger Impl - third commit

---
+++ app/src/main/java/com/project/moviam/MyApplication.java
@@ -0,0 +1,10 @@
+package com.project.moviam;
+
+import android.app.Application;
+
+import com.project.moviam.di.ApplicationComponent;
+
+public class MyApplication extends Application {
+
+   // ApplicationComponent applicationComponent=DaggerApplicationComponent.create();
+}
